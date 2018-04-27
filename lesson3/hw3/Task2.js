6f40153ef39925d675a62e43b8c177c0f58466e9
valeriya-reva <reva.valeri@gmail.com>
2018-04-27 08:03:58 +0300

    hw3

---
+++ lesson3/hw3/Task2.js
@@ -0,0 +1,16 @@
+/**
+ * Filter array. Only numbers that greater than 0 and less then 10 pass to result.
+ * @param {Array<number>} input
+ * @returns {Array<number>} Returns filtered array.
+//  */
+
+// В функцию передается массив с числами. Функция должна возвращать массив 
+// который содержит только те числа, которые больше нуля и меньше 10-ти.
+//  Для этого используйте вспомогательную функцию isNumberInRange, 
+//  которая проверяет, что оно больше нуля и меньше 10. Если это так - функция возвращает true, если не так - false.
+
+function advancedFilter(input) {
+    return;
+}
+
+module.exports = advancedFilter;
---
+++ lesson3/hw3/Task3.js
@@ -0,0 +1,16 @@
+/**
+ * Checks string is polindrome or not
+ * @param {string} input
+ * @returns {boolean} Returns is string polindroe or not.
+ */
+
+// В википедии прочитать что такое полиндромы. 
+// Функция должна принимать строку и выдавать true 
+// если строка является полндромом и false если она 
+// таковой не является. Игнорировать большие и малые буквы.
+
+function isPolindrome(input) {
+    return;
+}
+
+module.exports = isPolindrome;
