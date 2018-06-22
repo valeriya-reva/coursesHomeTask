cdb92053e8f6a82424221e9d4467555c3c502587
valeriya.reva <reva.valeri@gmail.com>
2018-06-22 18:06:41 +0300

    'promise'

--- lesson8/hw8/promise.js
+++ lesson8/hw8/promise.js
@@ -1,6 +1,5 @@
-
 var regV = /error/gi;
-var myStr;
+var myArr;
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,33 +21,35 @@ function apiFake (message, sec) {
 }
 
 Promise.all([
-    apiFake('one', getRandomArbitary(1, 10)), 
-    apiFake('two', getRandomArbitary(1, 10)), 
-    apiFake('three', getRandomArbitary(1, 10)), 
-    apiFake('four', getRandomArbitary(1, 10)), 
-    apiFake('five', getRandomArbitary(1, 10)), 
-    apiFake('six', getRandomArbitary(1, 10)),
-    apiFake('seven', getRandomArbitary(1, 10))])
+    apiFake('one', getRandomArbitary(1, 3)), 
+    apiFake('two', getRandomArbitary(1, 3)), 
+    apiFake('three', getRandomArbitary(1, 3)), 
+    apiFake('four', getRandomArbitary(1, 3)), 
+    apiFake('five', getRandomArbitary(1, 3)), 
+    apiFake('six', getRandomArbitary(1, 3)),
+    apiFake('seven', getRandomArbitary(1, 3))])
         .then((data) => {
-            myStr = data.join('');
-            return myStr;
+            myArr = data.join('');
+            return myArr;
+        })
+        .then((data) => {
             console.log(data);
+            apiFake(myArr + 'vvvvv', getRandomArbitary(1, 3))
+                .then((data) => {
+                    myArr = data;
+                    console.log(myArr);
+            })
+            return apiFake(myArr + 'vvvvv', getRandomArbitary(1, 3));
+        }).then((data) => {
+            var n = Math.round(data.length / 3);
+            var a = myArr.match(/.{1,13}/g);
+            
+            Promise.race([apiFake(a[0], getRandomArbitary(1, 3)), apiFake(a[1], getRandomArbitary(1, 3)), apiFake(a[2], getRandomArbitary(1, 3))])
+                .then((data) => {
+                    // console.log(data);
+                    return data;
+            })
         })
         .catch((err) => {
             console.log(err.message);
-        })
-
-
-        
-// apiFake('myStr', 2)
-//     .then((data) => {
-//         console.log(data);
-//     })
-//     .catch((err) => {
-//         console.log(err.message);
-//     })
-
-
-
-
-
+        });
